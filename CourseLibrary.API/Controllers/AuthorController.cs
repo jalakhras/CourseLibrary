a7ceca87ac140aa7878019d38fab4b93ef866608
Jassar Al-Akhras <JAlakhras@blessedtreeit.com>
2020-02-22 13:33:18 +0300

    Add Author Controller

---
+++ CourseLibrary.API/Controllers/AuthorController.cs
@@ -0,0 +1,32 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        public AuthorController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var autors = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(autors);
+        }
+
+        [HttpGet("{authorId:guid}")]
+        public IActionResult GetAuthors(Guid authorId)
+        {
+
+            var autors = _courseLibraryRepository.GetAuthor(authorId);
+            if (autors == null) return NotFound();
+            return new JsonResult(autors);
+        }
+    }
+}
--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -22,15 +22,15 @@ namespace CourseLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           services.AddControllers();
-             
+            services.AddControllers(setup=>setup.ReturnHttpNotAcceptable=true);
+
             services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
 
             services.AddDbContext<CourseLibraryContext>(options =>
             {
                 options.UseSqlServer(
                     @"Server=.;Database=CourseLibraryDB;Trusted_Connection=True;");
-            }); 
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
