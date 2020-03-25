be993e086ab1ddabaf6cfec2d6e86bb4a852ca5c
Jassar Al-Akhras <JAlakhras@blessedtreeit.com>
2020-03-25 23:01:55 +0300

    Class-level Input Validation with IValidatableObject

--- CourseLibrary.API/Model/CourseForCreationDto.cs
+++ CourseLibrary.API/Model/CourseForCreationDto.cs
@@ -1,10 +1,24 @@
-﻿namespace CourseLibrary.API.Model
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourseLibrary.API.Model
 {
-    public class CourseForCreationDto
+    public class CourseForCreationDto :IValidatableObject
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+        [MaxLength(500)]
         public string Description { get; set; }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(Title == Description )
+            {
+                yield return new ValidationResult("The provider description shold be deffranet from the title.", new[] { "CourseForCreationDto" });
+                
+            }
+        }
     }
 }
 
