d478958c47afdbb1efadc4ab5b1a4437c9c38d34
Jassar Al-Akhras <JAlakhras@blessedtreeit.com>
2020-03-28 19:19:46 +0300

    Customizing Error Messages

--- CourseLibrary.API/Model/CourseForCreationDto.cs
+++ CourseLibrary.API/Model/CourseForCreationDto.cs
@@ -1,24 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using CourseLibrary.API.ValidationAttributes;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Model
 {
-    public class CourseForCreationDto :IValidatableObject
+    [CourseTitleMustBeDifferentFromDescriptionAttributes(ErrorMessage ="The title must defferent from description. ")]
+    public class CourseForCreationDto //:IValidatableObject
     {
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage ="You should fill out title.")]
+        [MaxLength(100,ErrorMessage ="The title shouldn't have more than 1500 characters")]
         public string Title { get; set; }
-        [MaxLength(500)]
+        [MaxLength(500,ErrorMessage ="The description shouldn't have more than 1000 characters .")]
         public string Description { get; set; }
 
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if(Title == Description )
-            {
-                yield return new ValidationResult("The provider description shold be deffranet from the title.", new[] { "CourseForCreationDto" });
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if(Title == Description )
+        //    {
+        //        yield return new ValidationResult("The provider description shold be deffranet from the title.", new[] { "CourseForCreationDto" });
                 
-            }
-        }
+        //    }
+        //}
     }
 }
 
